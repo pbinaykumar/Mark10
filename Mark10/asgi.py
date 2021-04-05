9533705d985730b67fdf887298d37e8bbc7162bb
pbinaykumar19 <bkp@arl-tech.in>
2021-04-05 14:01:56 +0100

    'm'

--- Mark10/asgi.py
+++ Mark10/asgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
+
+
 import os
-
-from django.core.asgi import get_asgi_application
-
+import django
+from channels.routing import get_default_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mark10.settings')
-
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
