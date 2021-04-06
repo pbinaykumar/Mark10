6536f0a1e8fc2e1e7e6a70c96645aae9f048ae1f
pbinaykumar19 <bkp@arl-tech.in>
2021-04-06 11:10:45 +0100

    'static'

--- Mark10/urls.py
+++ Mark10/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
     path('document/', include('document.urls')),
     path('chat/', include('chattingapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
